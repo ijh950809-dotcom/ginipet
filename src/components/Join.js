4d4cdb6c7e964383387a01f3aefbefcb49070e72
ijh950809-dotcom <ijh950809@gmail.com>
2026-01-29 11:17:00 +0900

    Update Join.js

--- src/components/Join.js
+++ src/components/Join.js
@@ -33,7 +33,7 @@ function Join(props) {
 
   //4. 아이디 중복체크 기능
   const checkUsername = () => {
-    axios.post('http://localhost:9070/check-username', {
+    axios.post('https://port-0-backend-express-server-mkvwe8rkb7d9ea30.sel3.cloudtype.app/check-username', {
       username: form.username
     })
       .then(res => {
@@ -60,7 +60,7 @@ function Join(props) {
     }
 
     //비밀번호가 일치하면 서버측으로 내용을 전송
-    axios.post('http://localhost:9070/register', form)
+    axios.post('https://port-0-backend-express-server-mkvwe8rkb7d9ea30.sel3.cloudtype.app/ginipet_register', form)
       .then(() => {//전송성공시
         alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
         navigate('/login');//로그인 경로주소
@@ -156,5 +156,6 @@ function Join(props) {
     </main>
   );
 }
+
 
 export default Join;
